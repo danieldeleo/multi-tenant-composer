9c2c2cc7371743a272db1d496e4775f2974cf1e7
ddeleo <danieldeleo@google.com>
2022-02-22 14:20:54 -0500

    Updates

--- test/validate_dag.sh
+++ test/validate_dag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get install python3-venv -y
 python3 -m venv venv && source venv/bin/activate
 venv/bin/python -m pip install -r requirements.txt
 PYTHONPATH=/workspace venv/bin/python test/validate_dag.py
