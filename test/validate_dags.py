7a7988c6e3d4f5d295a4598c97334a37f66a357c
ddeleo <danieldeleo@google.com>
2022-02-22 17:47:18 -0500

    Updates

--- test/validate_dags.py
+++ test/validate_dags.py
@@ -5,6 +5,8 @@ from pathlib import Path
 dag_dir = Path('dags/')
 dags = dag_dir.glob('**/*.py')
 for dag in dags:
+  # Import dag source files directly
+  # https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
   spec = importlib.util.spec_from_file_location(dag.name, dag.absolute())
   module = importlib.util.module_from_spec(spec)
   sys.modules[dag.name] = module
