5b744e3e86dcea370789127cc47e3dca90ba2ea8
ddeleo <danieldeleo@google.com>
2022-02-22 12:13:10 -0500

    Updates

--- dags/multi_tenant_test.py
+++ dags/multi_tenant_test.py
@@ -15,7 +15,7 @@ default_args = {
 # for best practices
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
-TEAM_NAMESPACE = 'test6'
+TEAM_NAMESPACE = 'test7'
 
 with models.DAG(
     dag_id='multi_tenant_test',
